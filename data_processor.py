a09406391c081a2eb8a7411715d25556bbfcaf0d
Kolizty <2359115827@qq.com>
2022-11-21 03:14:27 +0800

    Update side info process

--- data_processor.py
+++ data_processor.py
@@ -65,7 +65,7 @@ def load_data():
         check_in = pd.read_csv(path + "checkins_" + city.lower() + "_follow_mat.csv")
         poi_data = pd.read_csv(path + "POI_" + city.lower() + "_follow_mat.csv")
 
-        print(city + "check_in number: ", len(check_in))
+        print(city + " check_in number: ", len(check_in))
         users = set(list(check_in['userid']))
         print(city + " user number: ", len(users))
         venues = set(list(check_in['Venue_id']))
@@ -102,6 +102,7 @@ def load_data():
         for i in range(len(venues_sort)):
             venues_dic[venues_sort[i]] = i + len(users_sort)
 
+        current_index = len(users_sort) + len(venues_sort) - 1
         # recode friendship
         friend_list_index = []
         for i in range(len(friend_list)):
@@ -151,6 +152,27 @@ def load_data():
             # check_in_local_year.append(localtime[0])
             check_in_local_month.append(localtime[1])
             check_in_hour_period.append(time_partition(localtime[6], localtime[3], localtime[4]))
+
+        time_hour_dic = {}
+        tot_hour_period = max(check_in_hour_period)
+
+        for i in range(len(check_in_hour_period)):
+            if check_in_hour_period[i] not in time_hour_dic:
+                time_hour_dic[check_in_hour_period[i]] = check_in_hour_period[i] + current_index
+            check_in_hour_period[i] = time_hour_dic[check_in_hour_period[i]]
+
+        current_index += tot_hour_period
+
+        time_month_dic = {}
+        tot_month = max(check_in_local_month)
+
+        for i in range(len(check_in_local_month)):
+            if check_in_local_month[i] not in time_month_dic:
+                time_month_dic[check_in_local_month[i]] = check_in_local_month[i] + current_index
+            check_in_local_month[i] = time_month_dic[check_in_local_month[i]]
+
+        current_index += tot_month
+
         # check_in['local_year'] = check_in_local_year
         check_in['local_month'] = check_in_local_month
         check_in['hour_period'] = check_in_hour_period
@@ -164,7 +186,7 @@ def load_data():
             df = df.reset_index(drop=True)
             user_trajectory_index[df['users_index'][0]] = set(list(df['venues_index']))
 
-        return check_in, friend_list_index, user_trajectory_index
+        return check_in, friend_list_index, user_trajectory_index, venues_dic, time_hour_dic, time_month_dic, current_index
 
     def time_partition(day, hour, min):
         # days [0-6] per 1 day, hours [0-23] per 1 hour, minutes [0-1] per 30 minutes
@@ -186,16 +208,33 @@ def load_data():
         pickle.dump(user_trajectory_index, f)
         f.close()
 
-    def load_extra_poi_info(city):
+    def load_extra_poi_info(city, venues, venues_dic, time_hour_dic, time_month_dic, current_index):
         base_dir = './data/raw/Venue_detail/'
-        file_names = os.listdir(base_dir + city + '/')
+        path = base_dir + city + '/'
+        file_names = os.listdir(path)
         file_names = zip(file_names, range(len(file_names)))
         file_names = dict(file_names)
-        all_side_info = []
+        all_side_info = {}
+
+        all_contact = {}
+        all_category_level_one = {}
+        all_category_level_two = {}
+
+        all_tip_count = []
+        all_price_tier = {}
+        all_like_count = []
+        all_rating = []
+        all_photos_count = []
+
+        tip_count_cut_num = 6
+        like_count_cut_num = 6
+        rating_cut_num = 6
+        photos_count_cut_num = 6
 
         for file_name in file_names:
             # load extra poi info from json file
-            with open(dir + file_name, 'r') as f:
+            with open(path + file_name, 'r') as f:
+
                 extra_poi_info = json.load(f)
                 side_info = {}
                 # exact useful poi information
@@ -205,47 +244,197 @@ def load_data():
                 # categories[{name, icon{prefix".../cate1/cate2"}}],
                 # stats{tipCount},
                 # price{tier},
-                # likes{count}, rating,
+                # likes{count},
+                # rating,
                 # photos{count},
                 # hours{timeframes[{days, open[{renderedtime}]}]}
                 # popular{timeframes[{days, open[{renderedtime}]}]}
-                id = extra_poi_info['id']
+                # if 'id' not in extra_poi_info:
+                #     # todo: 4ad83e6ff964a520eb1021e3.json airport 只有category
+                #     print(file_name)
+                #     continue
+                # id = extra_poi_info['id']
+                # if id == '4ad83e6ff964a520eb1021e3':
+                #     continue
+                #
+                #
+                if id not in venues or id in all_side_info:
+                    continue
 
-                name = extra_poi_info['name']
-                contact = extra_poi_info['contact'] if 'contact' in extra_poi_info.keys() else ''
-                address = extra_poi_info['location']['address'] if 'address' in extra_poi_info.keys() else ''
-                city = extra_poi_info['location']['city'] if 'city' in extra_poi_info.keys() else ''
-                country = extra_poi_info['location']['country'] if 'country' in extra_poi_info.keys() else ''
-                categories = extra_poi_info['categories'] if 'categories' in extra_poi_info.keys() else ''
-                stats = extra_poi_info['stats'] if 'stats' in extra_poi_info.keys() else ''
-                like_count = extra_poi_info['likes']['count'] if 'likes' in extra_poi_info.keys() else ''
-                rating = extra_poi_info['rating'] if 'rating' in extra_poi_info.keys() else ''
-                # print(id, name, contact, address, city, country, categories, stats, like_count, rating)
+                contacts_load = extra_poi_info['contact']
+                contacts_processed = set()
 
-                side_info.append({1: {id, name}})
-                side_info.append({2, {id, contact}})
-                side_info.append({3, {id, address}})
-                side_info.append({4, {id, city}})
-                side_info.append({5, {id, country}})
-                side_info.append({6, {id, categories}})
-                side_info.append({7, {id, stats}})
-                side_info.append({8, {id, like_count}})
-                side_info.append({9, {id, rating}})
+                for contact in contacts_load.keys():
+                    if contact == 'facebookUsername' or contact == 'facebookName' or contact == 'formattedPhone':
+                        continue
+                    if contact not in all_contact:
+                        all_contact[contact] = len(all_contact) + 1
+                    contacts_processed.add(all_contact[contact])
 
-                all_side_info.append(side_info)
+                categories = extra_poi_info['categories']
+                category_level_one_processed = set()
+                category_level_two_processed = set()
 
+                for category_info in categories:
+                    category_level_one = category_info['icon']['prefix'].split('/')[5]
+                    if category_level_one not in all_category_level_one:
+                        all_category_level_one[category_level_one] = len(all_category_level_one) + 1
+                    category_level_one_processed.add(all_category_level_one[category_level_one])
+
+                    category_level_two = category_info['name']
+                    if category_level_two not in all_category_level_two:
+                        all_category_level_two[category_level_two] = {}
+                        all_category_level_two[category_level_two]['index'] = len(all_category_level_two)
+                        all_category_level_two[category_level_two]['parent'] = all_category_level_one[
+                            category_level_one]
+                    category_level_two_processed.add(all_category_level_two[category_level_two]['index'])
+
+                tip_count = extra_poi_info['stats']['tipCount']
+                all_tip_count.append(tip_count)
+                # tip_count_processed = process_tip_count(tip_count)
+
+                price_tier = extra_poi_info['price']['tier'] if 'price' in extra_poi_info.keys() else None
+                if price_tier not in all_price_tier:
+                    all_price_tier[price_tier] = 1
+                else:
+                    all_price_tier[price_tier] += 1
+                price_tier_processed = 1 if price_tier is None else price_tier + 1
+
+                like_count = extra_poi_info['likes']['count'] if 'likes' in extra_poi_info.keys() else None
+                if like_count is None or like_count == -1:
+                    like_count = 0
+                all_like_count.append(like_count)
+
+                rating = extra_poi_info['rating'] if 'rating' in extra_poi_info.keys() else None
+                all_rating.append(rating)
+
+                photos_count = extra_poi_info['photos']['count']
+                all_photos_count.append(photos_count)
+
+                # todo: hours, popular
+
+                side_info['contact'] = contacts_processed
+                side_info['category_level_one'] = category_level_one_processed
+                side_info['category_level_two'] = category_level_two_processed
+                side_info['tip_count'] = tip_count
+                side_info['price_tier'] = price_tier_processed
+                side_info['like_count'] = like_count
+                side_info['rating'] = rating
+                side_info['photos_count'] = photos_count
+                all_side_info[venues_dic[id]] = side_info
+
+        all_tip_count = np.array(all_tip_count)
+        all_like_count = np.array(all_like_count)
+        all_rating = np.array(all_rating)
+        all_photos_count = np.array(all_photos_count)
+
+        tip_count_cut = cut_all_count(all_tip_count, cut_num=tip_count_cut_num)
+        like_count_cut = cut_all_count(all_like_count, cut_num=like_count_cut_num)
+        rating_cut = cut_all_count(all_rating[all_rating != None], cut_num=rating_cut_num)
+        photos_count_cut = cut_all_count(all_photos_count, cut_num=photos_count_cut_num)
+
+        for key in all_side_info.keys():
+            all_side_info[key]['tip_count'] = process_counts(all_side_info[key]['tip_count'], tip_count_cut)
+            all_side_info[key]['like_count'] = process_counts(all_side_info[key]['like_count'], like_count_cut)
+            if all_side_info[key]['rating'] is None:
+                all_side_info[key]['rating'] = 1
+            else:
+                all_side_info[key]['rating'] = process_counts(all_side_info[key]['rating'], rating_cut) + 1
+            all_side_info[key]['photos_count'] = process_counts(all_side_info[key]['photos_count'], photos_count_cut)
+
+        # recode side info
+
+        # print(current_index)
+        for key in all_side_info.keys():
+            all_side_info[key]['contact'] = {current_index + val for val in all_side_info[key]['contact']}
+
+        current_index += len(all_contact)
+
+        for key in all_category_level_one:
+            all_category_level_one[key] = current_index + all_category_level_one[key]
+
+        for key in all_category_level_two:
+            all_category_level_two[key]['parent'] = current_index + all_category_level_two[key]['parent']
+
+        for key in all_side_info.keys():
+            all_side_info[key]['category_level_one'] = {current_index + val for val in
+                                                        all_side_info[key]['category_level_one']}
+
+        current_index += len(all_category_level_one)
+
+        for key in all_category_level_two:
+            all_category_level_two[key]['index'] = current_index + all_category_level_two[key]['index']
+
+        for key in all_side_info.keys():
+            all_side_info[key]['category_level_two'] = {current_index + val for val in
+                                                        all_side_info[key]['category_level_two']}
+
+        current_index += len(all_category_level_two)
+
+        for key in all_side_info.keys():
+            all_side_info[key]['tip_count'] = current_index + all_side_info[key]['tip_count']
+
+        current_index += tip_count_cut_num
+
+        for key in all_side_info.keys():
+            all_side_info[key]['price_tier'] = current_index + all_side_info[key]['price_tier']
+
+        current_index += 5  # 5 price_tier in tot(1 is None)
+
+        for key in all_side_info.keys():
+            all_side_info[key]['like_count'] = current_index + all_side_info[key]['like_count']
+
+        current_index += like_count_cut_num
+
+        for key in all_side_info.keys():
+            all_side_info[key]['rating'] = current_index + all_side_info[key]['rating']
+
+        current_index += rating_cut_num
+
+        for key in all_side_info.keys():
+            all_side_info[key]['photos_count'] = current_index + all_side_info[key]['photos_count']
+
+        current_index += photos_count_cut_num
+
+        print("Total vertex number", current_index)
+
+        print(city + ' side information number: ', len(all_side_info))
         return all_side_info
+
+    def cut_all_count(all_count, cut_num):
+        all_count = np.sort(all_count)
+        cut_len = len(all_count) // cut_num
+        cuts = []
+        total = 0
+        for count in all_count:
+            total += 1
+            if total == cut_len:
+                cuts.append(count)
+                total = 0
+        return cuts
+
+    def process_counts(count, cuts):
+        index = 1
+        for cut in cuts:
+            if count > cut:
+                index += 1
+            else:
+                break
+        return index
 
     def load_all():
         # all_checkins, all_poi, all_friendships = load_basic_info()
         # all_checkin_relations = process_checkin(all_checkins)
-        cities = ['NYC', 'TKY', 'SP', 'JK']
+        cities = ['NYC', 'TKY', 'SP', 'JK', 'KL']
+        # cities = ['IST']  # 数据很多有问题，只有极少的信息
 
         for city in cities:
             check_in, poi_data, users, venues, friend = load_city_data(city)
-            check_in_processed, friend_processed, user_trajectory_processed = process_city_data(city, check_in, poi_data, users, venues, friend)
+            check_in_processed, friend_processed, user_trajectory_processed, venues_dic, time_hour_dic, time_month_dic, current_index \
+                = process_city_data(city, check_in, poi_data, users, venues, friend)
             # dump_city_data(city, check_in_processed, friend_processed, user_trajectory_processed)
-            # poi_details = load_extra_poi_info(city)
+
+            poi_details = load_extra_poi_info(city, venues, venues_dic, time_hour_dic, time_month_dic, current_index)
 
     load_all()
     print('load success')
